66b3cf6658d43c6866ec559d1493bb627bd1402b
weizhoulan <weizhou.lan@daocloud.io>
2019-03-05 22:12:38 +0800

    modify locaiton of conf file

--- image/service/keepalived/process.sh
+++ image/service/keepalived/process.sh
@@ -4,12 +4,12 @@
 # https://github.com/osixia/docker-light-baseimage/blob/stable/image/tool/log-helper
 log-helper level eq trace && set -x
 
-echo -n "Waiting config file /usr/local/etc/keepalived/keepalived.conf"
-while [ ! -e "/usr/local/etc/keepalived/keepalived.conf" ]
+echo -n "Waiting config file /etc/keepalived/keepalived.conf"
+while [ ! -e "/etc/keepalived/keepalived.conf" ]
 do
   echo -n "."
   sleep 0.1
 done
 echo "ok"
 
-exec /usr/local/sbin/keepalived -f /usr/local/etc/keepalived/keepalived.conf --dont-fork --log-console ${KEEPALIVED_COMMAND_LINE_ARGUMENTS}
+exec /usr/local/sbin/keepalived -f /etc/keepalived/keepalived.conf --dont-fork --log-console ${KEEPALIVED_COMMAND_LINE_ARGUMENTS}
